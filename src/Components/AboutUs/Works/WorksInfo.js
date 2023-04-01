ce0d69cc649413466cbe5fd7a06d240db203dd61
jainam panchal <jainampancha345@gmail.com>
2023-04-01 12:13:51 +0530

    work section uploaded

---
+++ src/Components/AboutUs/Works/WorksInfo.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./WorksInfo.css";
+import Info_image from "../../../Images/HeroImage-removebg-preview.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "@mui/material";
+
+const Info = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <h2 className="work_title text-center p-2 ">
+          We are an NGO dedicated to improving the lives of children in
+          underprivileged communities.
+        </h2>
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 col-12 p-4">
+            <h3 className="text-left">Why Prayatna ?</h3>
+            <h4>Together, let's help each other </h4>
+            <p className="work_info">
+              <spane>
+                We visited various NGO websites and found that they were doing
+                only certain activities.
+                <br />
+                They have faced the issue of finding a provider or NGO as per
+                their needs.
+                <br />
+                people must have to visit different NGO to take help from them.
+                <br />
+                People have to visit various NGO websites to find the perfect
+                one.
+              </spane>
+            </p>
+            <p className="work_info">
+              Our work would not be possible without the support of our donors,
+              volunteers, and staff. We are grateful for their commitment to our
+              cause and their dedication to making a difference in the lives of
+              children.
+            </p>
+            <Link to="contact" smooth={true} spy={true}>
+              <button type="button" class="work_btn btn-secondary btn-sm">
+                MORE ACTIVITY
+              </button>
+            </Link>
+          </div>
+          <div className="col-sm-12 col-lg-6 col-12 p-4 ">
+            <img src={Info_image} alt="info_image" className="work-pic" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Info;
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,14 +3,16 @@ import Navbar from "../Components/Navbar/Navbar";
 import Hero from "../Components/AboutUs/Hero/HeroSection";
 import Team from "../Components/AboutUs/Team/team";
 import Info from "../Components/AboutUs/Information/Info";
+import Work from "../Components/AboutUs/Works/WorksInfo";
 
 const About = () => {
   return (
     <>
       <Navbar />
-      <Hero/>
-      <Info/>
-      <Team/>
+      <Hero />
+      <Work />
+      <Info />
+      <Team />
     </>
   );
 };
